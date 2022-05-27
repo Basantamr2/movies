6f0a03feead6729018cb7a3e4004f04f30c5499d
unknown <basant.amr.512000@gmail.com>
2022-05-27 16:57:56 +0200

    movies

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'screens/aspect_ratio/view.dart';
+ import 'screens/confetti_animation/view.dart';
+ import 'screens/flip_paper/view.dart';
+ import 'screens/neon_button/view.dart';
+import 'screens/movie_db/view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      builder: (BuildContext context, Widget? child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: PageView(
+          children: [
+            MoviesScreen(),
+            AspectRatioScreen(),
+            ConfettiScreen(),
+            FlipPaperScreen(),
+            NeonButtonScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/movie_db/controller.dart
@@ -0,0 +1,14 @@
+import 'package:movies/screens/movie_db/model.dart';
+import 'package:dio/dio.dart';
+
+class MovieDBController {
+  MoviesData? model;
+
+  Future<void> getMovies() async {
+    var response = await Dio().get(
+        "https://api.themoviedb.org/3/discover/movie?api_key=2001486a0f63e9e4ef9c4da157ef37cd&page=9");
+    model = MoviesData.fromJson(response.data);
+    // print(model!.movies.length);
+    // print(response.data);
+  }
+}
